fad982dc173239908938d84cc1d6d38619ccf93c
NoahLewisDaly <116974774+NoahLewisDaly@users.noreply.github.com>
2023-08-30 22:37:33 -0500

    Added getting top songs by artist.

--- app.py
+++ app.py
@@ -31,21 +31,28 @@ def get_auth_header(token):
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f"q={artist_name}&type=artist&limit=1" # type could also be artist,track and limit is how many results appear
+    query = f"?q={artist_name}&type=artist&limit=1" # type could also be artist,track and limit is how many results appear
 
     query_url = url + query
     result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)
-    print(json_result)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No Artist with this name exists.")
+        return None
+    return json_result[0]
 
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers = headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
 
 token = get_token()
-search_for_artist(token, "ACDC")
+result = search_for_artist(token, "Drake")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
 
-# app = Flask(__name__)
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
 
-# @app.route("/")
-# def index():
-#     return "Drink more coffee RN"
-
-# app.run(host="0.0.0.0", port=80)
